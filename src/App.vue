f8ff6780eda7470d892a73bcf5059c5dfefd94a7
ylh <345072986@qq.com>
2017-12-22 15:25:38 +0800

    项目初始化，recommend module start

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <m-header></m-header>
+        <tab></tab>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+    import MHeader from "components/m-header/m-header.vue"
+    import Tab from "components/tab/tab.vue"
+
+    export default {
+        name: 'app',
+        components: {
+            MHeader,
+            Tab
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+    @import "~common/stylus/variable";
+
+    #app
+        color: $color-theme
 </style>
--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,24 +1,26 @@
 import originJsonp from 'jsonp'
 
 export default function jsonp(url, data, option) {
-  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+    url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
 
-  return new Promise((resolve, reject) => {
-    originJsonp(url, option, (err, data) => {
-      if (!err) {
-        resolve(data)
-      } else {
-        reject(err)
-      }
+    return new Promise((resolve, reject) => {
+        // jsonp库，回调一个err和data
+        originJsonp(url, option, (err, data) => {
+            if (!err) {
+                resolve(data)
+            } else {
+                reject(err)
+            }
+        })
     })
-  })
 }
 
 export function param(data) {
-  let url = ''
-  for (var k in data) {
-    let value = data[k] !== undefined ? data[k] : ''
-    url += '&' + k + '=' + encodeURIComponent(value)
-  }
-  return url ? url.substring(1) : ''
+    let url = ''
+    for (var k in data) {
+        let value = data[k] !== undefined ? data[k] : ''
+        url += '&' + k + '=' + encodeURIComponent(value)
+    }
+    // 删除第一个&
+    return url ? url.substring(1) : ''
 }
