152b3574e83a79ffcfc5c354a96ba85648fc569f
ylh <345072986@qq.com>
2018-01-17 18:29:49 +0800

    singer-detail，player-start

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,38 +1,68 @@
 <template>
     <transition name="slide">
-        <div class="singer-detail">
-
-        </div>
+        <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
     </transition>
 </template>
 
 <script>
     import {mapGetters} from "vuex"
+    import {getSingerDetail} from "api/singer"
+    import {ERR_OK} from "api/config"
+    import {createSong} from "common/js/song"
+    import MusicList from "components/music-list/music-list"
 
     export default {
         data() {
-            return {}
+            return {
+                songs: [],
+            }
         },
-        computed:{
+        computed: {
+            title() {
+                return this.singer.name;
+            },
+            bgImage() {
+                return this.singer.avatar;
+            },
             ...mapGetters([
-                
+                'singer',
             ])
         },
-        methods: {}
+        created() {
+            this._getDetail();
+        },
+        methods: {
+            _getDetail() {
+                if (!this.singer.id) {
+                    this.$router.push('/singer');
+                    return;
+                }
+                getSingerDetail(this.singer.id).then((res) => {
+                    if (res.code == ERR_OK) {
+                        this.songs = this._normalizeSongs(res.data.list);
+                    }
+                })
+            },
+            _normalizeSongs(list) {
+                let ret = [];
+                list.forEach((item) => {
+                    let {musicData} = item;
+                    if (musicData.songid && musicData.albummid) {
+                        ret.push(createSong(musicData));
+                    }
+                })
+                return ret;
+            }
+        },
+        components: {
+            MusicList
+        }
     }
 </script>
 
 <style scoped lang="stylus">
     @import "~common/stylus/variable"
 
-    .singer-detail
-        position: fixed;
-        z-index: 100
-        top: 0
-        left: 0
-        bottom: 0
-        right: 0
-        background: $color-background
     .slide-enter-active, .slide-leave-active
         transition: all 0.3s
 
